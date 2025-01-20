42942809e392759ca4f2a8ac67c3c09d075359c4
Junk-debug <maks.naumovich@gmail.com>
2025-01-20 10:10:55 +0100

    feat: add logic to fit level 3, add ability to make up to 3 requests at a time with their own state, when all request are loaded, user can make new request that will override first loaded request in array

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,88 @@ import { useState } from "react";
 import { Button } from "@/ui/button";
 import { Check, LoaderCircle, TriangleAlert } from "lucide-react";
 import { saveFile } from "@/lib/server";
+import { v4 as uuid } from "uuid";
+
+type Request = {
+  id: string;
+  loading: boolean;
+  error: string | null;
+  data: string | null;
+};
+
+const initRequest = (): Request => ({
+  id: uuid(),
+  loading: true,
+  error: null,
+  data: null,
+});
 
 function App() {
-  const [message, setMessage] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [activeRequests, setActiveRequests] = useState(0);
-  const loading = activeRequests > 0;
+  const [requests, setRequests] = useState<Request[]>([]);
+  const activeRequests = requests.filter((req) => req.loading).length;
 
   const sendRequest = async () => {
-    setActiveRequests((prev) => prev + 1);
-    setMessage(null);
-    setError(null);
+    if (activeRequests >= 3) return;
+
+    const newRequest = initRequest();
+
+    const firstCompleted = requests.find((req) => !req.loading);
+    if (firstCompleted) {
+      setRequests((prev) =>
+        prev.map((req) => (req.id === firstCompleted.id ? newRequest : req))
+      );
+    } else {
+      setRequests((prev) => [...prev, newRequest]);
+    }
 
     try {
       const result = await saveFile();
       console.log(result);
-      setMessage(result);
+      setRequests((prev) =>
+        prev.map((req) =>
+          req.id === newRequest.id ? { ...req, data: result } : req
+        )
+      );
     } catch (err: unknown) {
       console.error(err);
-      if (typeof err === "string") setError(err);
+      if (typeof err === "string")
+        setRequests((prev) =>
+          prev.map((req) =>
+            req.id === newRequest.id ? { ...req, error: err } : req
+          )
+        );
     } finally {
-      setActiveRequests((prev) => prev - 1);
+      setRequests((prev) =>
+        prev.map((req) =>
+          req.id === newRequest.id ? { ...req, loading: false } : req
+        )
+      );
+    }
+  };
+
+  const retry = async (id: Request["id"]) => {
+    setRequests((prev) =>
+      prev.map((req) =>
+        req.id === id ? { ...req, data: null, error: null, loading: true } : req
+      )
+    );
+
+    try {
+      const result = await saveFile();
+      console.log(result);
+      setRequests((prev) =>
+        prev.map((req) => (req.id === id ? { ...req, data: result } : req))
+      );
+    } catch (err: unknown) {
+      console.error(err);
+      if (typeof err === "string")
+        setRequests((prev) =>
+          prev.map((req) => (req.id === id ? { ...req, error: err } : req))
+        );
+    } finally {
+      setRequests((prev) =>
+        prev.map((req) => (req.id === id ? { ...req, loading: false } : req))
+      );
     }
   };
 
@@ -37,26 +98,34 @@ function App() {
           disabled={activeRequests >= 3}
           onClick={async () => sendRequest()}
         >
-          {error ? "Try again" : "Save File"}
+          Save File
         </Button>
         <span className="text-white">Active requests: {activeRequests}</span>
-        {loading && <LoaderCircle className="animate-spin text-white" />}
-        {message && (
-          <div className="w-full max-w-96 bg-white rounded-lg shadow-lg border-l-4 border-green-500 p-4">
-            <span className="animate-appear flex items-center gap-1 mb-2 text-green-600 text-xl font-semibold">
-              <Check /> Success
-            </span>
-            <p className="text-sm text-gray-600">{message}</p>
-          </div>
-        )}
-        {error && (
-          <div className="w-full max-w-96 bg-white rounded-lg shadow-lg border-l-4 border-red-600 p-4">
-            <span className="animate-appear flex items-center gap-1 mb-2 text-red-600  text-xl font-semibold">
-              <TriangleAlert /> Error
-            </span>
-            <p className="text-sm text-gray-600">{error}</p>
-          </div>
-        )}
+        <div>
+          {requests.map(({ loading, data, error, id }) => (
+            <div key={id}>
+              {id}
+              {loading && <LoaderCircle className="animate-spin text-white" />}
+              {data && (
+                <div className="w-full max-w-96 bg-white rounded-lg shadow-lg border-l-4 border-green-500 p-4">
+                  <span className="animate-appear flex items-center gap-1 mb-2 text-green-600 text-xl font-semibold">
+                    <Check /> Success
+                  </span>
+                  <p className="text-sm text-gray-600">{data}</p>
+                </div>
+              )}
+              {error && (
+                <div className="w-full max-w-96 bg-white rounded-lg shadow-lg border-l-4 border-red-600 p-4">
+                  <span className="animate-appear flex items-center gap-1 mb-2 text-red-600  text-xl font-semibold">
+                    <TriangleAlert /> Error
+                  </span>
+                  <p className="text-sm text-gray-600">{error}</p>
+                  <Button onClick={() => retry(id)}>Retry</Button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
